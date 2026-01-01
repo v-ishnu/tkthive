7a338ad0c2bdc7b20324a4137a06d2ddc73eda20
v-ishnu <vishnu.praaksh@gmail.com>
2026-01-01 23:23:28 +0530

    Fix Routing issue

--- src/modules/organizers/organizer.routes.js
+++ src/modules/organizers/organizer.routes.js
@@ -3,8 +3,8 @@ import protect from "../../lib/middleware/protect.middleware.js";
 import { createOrganizerController } from "./controller/org.create.js";
 import { inviteStaffController } from "./controller/inviteOwner.controller.js";
 import { authorize } from "../../lib/middleware/authorize.middleware.js";
-import { getMyOrganizers, getOrganizerController } from "./controller/Admin.getOrgcontroller.js";
-// import { getOrganizerController } from "./controller/admin.getOrgcontroller.js";
+import { getMyOrganizers } from "./controller/Admin.getOrgcontroller.js";
+import { getOrganizerController } from "./controller/user.getOrgcontroller.js";
 
 const organizerRouter = Router();
 
