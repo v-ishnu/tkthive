ef0868a8b0d7aaa6175e77655eb4e0af973050d6
Abhishek Kumar <meaviishek@gmail.com>
2026-02-16 20:41:19 +0530

    ch

--- src/app.js
+++ src/app.js
@@ -27,18 +27,18 @@ app.use(cors({
   exposeHeaders: ["set-cookie"]
 }));
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per window
-  message: {
-    success: false,
-    message: "Too many requests, please try again later."
-  },
-  standardHeaders: true,
-  legacyHeaders: false
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // limit each IP to 100 requests per window
+//   message: {
+//     success: false,
+//     message: "Too many requests, please try again later."
+//   },
+//   standardHeaders: true,
+//   legacyHeaders: false
+// });
 
-app.use(limiter);
+// app.use(limiter);
 
 
 /**
